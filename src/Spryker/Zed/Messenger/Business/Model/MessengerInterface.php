50b85791ed23b38807658f449c1d1e8678dc4c3c
René Klatt <rene.klatt@spryker.com>
2016-01-11 10:27:44 +0000

    #1064 moved MessengerInterface to MessengerBundle

---
+++ src/Spryker/Zed/Messenger/Business/Model/MessengerInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * (c) Spryker Systems GmbH copyright protected
+ */
+
+namespace Spryker\Zed\Messenger\Business\Model;
+
+use Psr\Log\LoggerInterface;
+
+interface MessengerInterface extends LoggerInterface
+{
+}
